4f671e354ed7c517ba7e4ea582cfb5a389e6f07f
DivoCodes <david.wageeh@gmail.com>
2023-08-25 01:41:46 +0300

    Added Header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,5 +1,32 @@
+// next
+import Image from "next/image";
+import Link from "next/link";
+
+// components
+import Socials from "@/app/components/Socials";
+
 const Header = () => {
-  return <header className="absolute z-30 w-full">Header</header>;
+  return (
+    <header className="absolute z-30 w-full flex justify-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 ">
+          {/* logo */}
+          <Link href={"/"}>
+            <Image
+              src={"/logo.svg"}
+              width={220}
+              height={48}
+              alt=""
+              priority={true}
+            />
+          </Link>
+
+          {/* socials */}
+          <Socials />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
