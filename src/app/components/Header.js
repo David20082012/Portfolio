b0f75e073c16a346f430c83f472ae717d0ce79d6
DivoCodes <david.wageeh@gmail.com>
2023-08-28 22:56:19 +0300

    fixed all images

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
           {/* logo */}
           <Link href={"/"}>
             <Image
-              src={"/logo.svg"}
+              src={"./logo.svg"}
               width={220}
               height={48}
               alt=""
