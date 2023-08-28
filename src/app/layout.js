2c26d6b1fe45bff1e7cb0e3049f339f28b402145
DivoCodes <david.wageeh@gmail.com>
2023-08-29 01:13:18 +0300

    fixed explosion image 4

--- src/app/layout.js
+++ src/app/layout.js
@@ -33,7 +33,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`page bg-[url(${someImage})] text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
+        className={`page bg-[url('/public/site-bg.svg')] text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}
       >
         <AnimatePresence mode="wait">
           <motion.div key={router.route} className="h-full">
